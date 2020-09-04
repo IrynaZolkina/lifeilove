2574f7e3717d3e9d4f67361b976f79c55f6bcc55
IrynaZolkina <wzyz7557@hotmail.com>
2020-09-04 14:43:12 -0700

    Updates

---
+++ precache-manifest.2d50b5ae9b2d2c0b342a6ee4c2c7b2c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c294a128d42903a9eeae476745858c",
+    "url": "/lifeilove/index.html"
+  },
+  {
+    "revision": "a5d17cb73b053757417d",
+    "url": "/lifeilove/static/css/main.9ff15cde.chunk.css"
+  },
+  {
+    "revision": "524222a44f9b9099fe40",
+    "url": "/lifeilove/static/js/2.1d6e7253.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/lifeilove/static/js/2.1d6e7253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d17cb73b053757417d",
+    "url": "/lifeilove/static/js/main.63f2905d.chunk.js"
+  },
+  {
+    "revision": "ea6fa968c9dced155e7b",
+    "url": "/lifeilove/static/js/runtime-main.3f7c5ded.js"
+  },
+  {
+    "revision": "ac2107c16046c9aad2284bf0be7b8184",
+    "url": "/lifeilove/static/media/cart.ac2107c1.png"
+  },
+  {
+    "revision": "74379ac2f645128ac61c0bbe52a3f3f4",
+    "url": "/lifeilove/static/media/eeernbg.74379ac2.png"
+  }
+]);
